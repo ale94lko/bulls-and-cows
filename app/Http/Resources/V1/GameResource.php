7d72be422dd1e4756caaf2f8ff2f6b40fb15b827
Fidel Alejandro Fernández Arias <47539231+ale94lko@users.noreply.github.com>
2024-01-08 12:22:34 -0500

    Add endpoints to get basic data from games

---
+++ app/Http/Resources/V1/GameResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GameResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user' => $this->user,
+            'age' => $this->age,
+            'maxTime' => $this->max_time,
+            'status' => $this->status,
+        ];
+    }
+}
